b2e79f8135e877f51667d457bcf4fa23830baee0
pankona <yosuke.akatsuka@gmail.com>
2021-02-22 23:51:09 +0900

    show simple auth screen

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import firebase from "./firebase";
 
-const App: React.FunctionComponent = () => {
-  return <div>Hello simple FunctionComponent!</div>;
+const App: React.FC = () => {
+  const [user, setUser] = useState<firebase.User | null>(null);
+
+  useEffect(() => {
+    return firebase.auth().onAuthStateChanged((user) => {
+      setUser(user);
+    });
+  }, []);
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+  };
+
+  return (
+    <div className="App">
+      <p className="App-intro">Name: {user && user.displayName}</p>
+
+      {user ? (
+        <button onClick={logout}>Google Logout</button>
+      ) : (
+        <button onClick={login}>Google Login</button>
+      )}
+    </div>
+  );
 };
 
 export default App;
