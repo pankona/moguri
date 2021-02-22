82b6962bd4fc94090af867dce795ae26cd108fc1
pankona <yosuke.akatsuka@gmail.com>
2021-02-22 23:02:54 +0900

    remove watch from build

--- build.ts
+++ build.ts
@@ -11,12 +11,6 @@ build({
   bundle: true,
   minify: !isDev,
   sourcemap: isDev,
-  watch: {
-    onRebuild(error, result) {
-      if (error) console.error("watch build failed:", error);
-      else console.error("watch build succeeded:", result);
-    },
-  },
 }).catch((err) => {
   console.log("Error:" + JSON.stringify(err));
 });
