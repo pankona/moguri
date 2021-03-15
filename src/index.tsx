5410477a3c2309346fa2ff6d5be739230eff4092
pankona <yosuke.akatsuka@gmail.com>
2021-03-15 23:21:08 +0900

    temporary disable ress

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
-import "ress";
+//import "ress";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
