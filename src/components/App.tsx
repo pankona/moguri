555224ee07acab0b99872a44530c5a6647109db9
pankona <yosuke.akatsuka@gmail.com>
2021-03-16 00:35:49 +0900

    remove unused import

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Character, CharacterState, Dangeon } from "./Character";
+import { Character } from "./Character";
 import { CharacterStoreMemory } from "./CharacterStore";
 import DangeonScene from "./Dangeon";
 import firebase from "./firebase";
